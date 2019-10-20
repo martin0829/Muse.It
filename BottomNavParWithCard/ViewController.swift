19cd623ac19da6d208bb828c53d37e2edd4f8a1e
Martin Kim <minty0829@berkeley.edu>
2019-10-20 12:34:33 -0700

    Initial Commit

---
+++ BottomNavParWithCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BottomNavParWithCard
+//
+//  Created by Martin Kim on 10/20/19.
+//  Copyright © 2019 Martin Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
